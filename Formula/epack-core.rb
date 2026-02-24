bdede1d39c7381fa883156b3d4d6a202cb3dc39b
Michael Nipper <mjn4406@gmail.com>
2026-02-24 13:55:57 -0500

    epack 0.1.4

--- Formula/epack-core.rb
+++ Formula/epack-core.rb
@@ -2,18 +2,18 @@ class EpackCore < Formula
   desc "CLI for creating and verifying evidence packs (core: no components)"
   homepage "https://github.com/locktivity/epack"
   license "Apache-2.0"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     on_arm do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-core-darwin-arm64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-core-darwin-arm64"
       sha256 "a6c470be548b033d2db1d0f19c09a8a0c4f933248b5c04dd1834822ebaa7d865"
       def install
         bin.install "epack-core-darwin-arm64" => "epack-core"
       end
     end
     on_intel do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-core-darwin-amd64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-core-darwin-amd64"
       sha256 "debc948437e1609b58e631f66a0b85beb31dc2536bc34f5eeba0a6d27651559a"
       def install
         bin.install "epack-core-darwin-amd64" => "epack-core"
@@ -23,14 +23,14 @@ class EpackCore < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-core-linux-arm64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-core-linux-arm64"
       sha256 "1cfee096f6853edec57f9e42415bd119aa78ff1b043efbd2c4eeffb99cd71eaf"
       def install
         bin.install "epack-core-linux-arm64" => "epack-core"
       end
     end
     on_intel do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-core-linux-amd64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-core-linux-amd64"
       sha256 "9e0177b5355fa1c3c496fbd1509da14b3d036249650a888453e428683b9b54c3"
       def install
         bin.install "epack-core-linux-amd64" => "epack-core"
--- Formula/epack.rb
+++ Formula/epack.rb
@@ -2,18 +2,18 @@ class Epack < Formula
   desc "CLI for creating and verifying evidence packs (full: components)"
   homepage "https://github.com/locktivity/epack"
   license "Apache-2.0"
-  version "0.1.3"
+  version "0.1.4"
 
   on_macos do
     on_arm do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-darwin-arm64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-darwin-arm64"
       sha256 "9926b96a506eb1747c5928fb0c21e72b3a3c4826479032c8c8ccfbde3aea37e9"
       def install
         bin.install "epack-darwin-arm64" => "epack"
       end
     end
     on_intel do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-darwin-amd64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-darwin-amd64"
       sha256 "26f0f0b97d37808bffe475a793a49ef28193387a58756184198720d4b664f0a4"
       def install
         bin.install "epack-darwin-amd64" => "epack"
@@ -23,14 +23,14 @@ class Epack < Formula
 
   on_linux do
     on_arm do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-linux-arm64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-linux-arm64"
       sha256 "2500b137e405750114a6316ae2c1931134563f9ad2d6624abc1a3212e3425b9b"
       def install
         bin.install "epack-linux-arm64" => "epack"
       end
     end
     on_intel do
-      url "https://github.com/locktivity/epack/releases/download/v0.1.3/epack-linux-amd64"
+      url "https://github.com/locktivity/epack/releases/download/v0.1.4/epack-linux-amd64"
       sha256 "a194c683974caecea848738834da4ee159b0459bef3d70344ff15d13a7903555"
       def install
         bin.install "epack-linux-amd64" => "epack"
